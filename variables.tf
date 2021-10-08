4791e4ea339602f53a0f1ee784ca4ed789360e01
Meghashree N <meghashreetendulkar@gmail.com>
2021-10-08 10:07:53 +0530

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,34 +1,11 @@
-variable "tenancy_ocid" {
-}
-
-variable "compartment_ocid" {
-}
-
-variable "user_ocid" {
-}
-
-variable "fingerprint" {
-}
-
-variable "private_key_path" {
-}
-
-variable "region" {
-}
-
-variable "region2" {
-}
-
-#declare the providers where you want to create resources
+#define oci provider configuaration
 provider "oci" {
-  alias            = "home"
   tenancy_ocid     = var.tenancy_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
   user_ocid        = var.user_ocid
+  region           = var.region
+  private_key_path = var.private_key_path
+  fingerprint      = var.fingerprint
 }
-
 provider "oci" {
   alias            = "region2"
   tenancy_ocid     = var.tenancy_ocid
@@ -37,11 +14,34 @@ provider "oci" {
   region           = var.region2
   user_ocid        = var.user_ocid
 }
+#provide the list of availability domain
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.compartment_ocid
+}
+
+#common variables
+variable "tenancy_ocid" {
+}
+
+variable "user_ocid" {
+}
+
+variable "private_key_path" {
+}
+
+variable "fingerprint" {
+}
+
+variable "region" {
+}
+
+variable "compartment_ocid" {
+}
 
 #variables to define vcn
 variable "vcn_cidr_block" {
   description = "provide the valid IPV4 cidr block for vcn"
-  default     = "192.168.0.0/16"
+  #default = "192.168.0.0/16"
 }
 
 variable "vcn_dns_label" {
@@ -51,6 +51,56 @@ variable "vcn_dns_label" {
 
 variable "vcn_display_name" {
   description = "provide a display name for vcn"
-  default     = "vcn_by_terraform"
 }
 
+#variables to define the public subnet
+variable "cidr_block_publicsubnet" {
+  description = "note that the cidr block for the subnet must be smaller and part of the vcn cidr block"
+  #default = "192.168.1.0/24"
+}
+
+variable "publicSubnet_dns_label" {
+  description = "A DNS label prefix for the subnet, used in conjunction with the VNIC's hostname and VCN's DNS label to form a fully qualified domain name (FQDN) for each VNIC within this subnet. "
+  default     = "publicsubnet"
+}
+
+variable "display_name_publicsubnet" {
+  description = "privide a displayname for public subnet"
+}
+
+#variables to create linux instance
+variable "instance_image_ocid" {
+  type = map(string)
+
+  default = {
+    ap-sydney-1  = "ocid1.image.oc1.ap-sydney-1.aaaaaaaao3vineoljixw657cxmbemwasdgirfy6yfgaljqsvy2dq7wzj2l4q"
+    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaahjkmmew2pjrcpylaf6zdddtom6xjnazwptervti35keqd4fdylca"
+    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaav3isrmykdh6r3dwicrdgpmfdv3fb3jydgh4zqpgm6yr5x3somuza"
+    ap-mumbai-1  = "ocid1.image.oc1.ap-mumbai-1.aaaaaaaanwfcq3baulkm73kcimzymx7qgfpoja2b56wgwhopjjgrz4om67zq"
+  }
+  # Updated to Oracle Linux 7.8 with all patches as of April 2020.
+}
+
+variable "instance_name" {
+  description = "provide the display name for thelinux instance to be deployed"
+}
+
+variable "ssh_authorized_keys" {
+  default = "C:/Users/megn/new.pub"
+}
+
+variable "preserve_boot_volume" {
+  default = false
+}
+
+variable "boot_volume_size_in_gbs" {
+  default = "50"
+}
+
+variable "shape" {
+  default = "VM.Standard2.1"
+}
+
+variable "assign_public_ip" {
+  default = true
+}
