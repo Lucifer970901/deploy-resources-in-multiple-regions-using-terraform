cd19db989ec19215cb8e4b3b42ee7a4575aadd2f
Meghashree N <meghashreetendulkar@gmail.com>
2021-10-12 12:01:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -72,7 +72,7 @@ variable "display_name_publicsubnet" {
 
 #variables to create linux instance
 variable "instance_image_ocid" {
-  type = tomap(string)
+  type = map(string)
 
   default = {
     ap-sydney-1  = "ocid1.image.oc1.ap-sydney-1.aaaaaaaao3vineoljixw657cxmbemwasdgirfy6yfgaljqsvy2dq7wzj2l4q"
