91bedc408c3a5371adb9cbaa4df22a82cccb90c9
Meghashree N <meghashreetendulkar@gmail.com>
2021-10-12 11:48:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 #define oci provider configuaration
 provider "oci" {
+  alias            = "home"
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   region           = var.region
